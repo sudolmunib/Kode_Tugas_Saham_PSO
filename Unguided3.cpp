314f5ac61805b1ed53945a14d37d104e817e4aa8
sudolmunib <162574378+sudolmunib@users.noreply.github.com>
2024-04-08 08:13:39 +0700

    Add files via upload

---
+++ Unguided3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 10;
+    int data[n] = {9, 4, 1, 4, 7, 10, 5, 4, 12, 4};
+    int cari = 4; 
+    int count = 0; 
+    int i;
+    
+    for (i = 0; i < n; i++) {
+        if (data[i] == cari) {
+            count++; 
+        }
+    }
+    
+    cout << "\t Program Sequential Search Sederhana\n" << endl;
+    cout << " data: {9, 4, 1, 4, 7, 10, 5, 4, 12, 4}" << endl;
+    
+    cout << "\n angka " << cari << " muncul sebanyak " << count << " kali." << endl;
+    
+    return 0;
+}
